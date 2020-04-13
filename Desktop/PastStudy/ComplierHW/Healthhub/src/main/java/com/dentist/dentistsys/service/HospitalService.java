bbbd791f5b7763f85f0b9f96fa706b493007cb45
sonnegod <bencerbins@gmail.com>
2020-04-13 12:41:16 -0500

    hospital service fix

--- Desktop/PastStudy/ComplierHW/Healthhub/src/main/java/com/dentist/dentistsys/service/HospitalService.java
+++ Desktop/PastStudy/ComplierHW/Healthhub/src/main/java/com/dentist/dentistsys/service/HospitalService.java
@@ -19,7 +19,7 @@ public class HospitalService {
                 hospitalInfoExample = new hospitalInfoExample();
                 com.dentist.dentistsys.entity.hospitalInfoExample.Criteria c = hospitalInfoExample.createCriteria();
                 c.andHospitalidEqualTo(ID).andFirstnameEqualTo(firstName).andLastnameEqualTo(lastName);
-                if (Physicain.length()>0){
+                if (Physicain.compareTo("null") != 0){
                     System.out.println("_______________+++++++++"+Physicain);
                     c.andDoctornameEqualTo(Physicain);
                 }
